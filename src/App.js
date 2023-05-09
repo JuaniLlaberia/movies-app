d1c674e813f2ab206f5db48f45a4a8d0bee9a606
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-05-09 10:32:50 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  useEffect(()=> {
+    const fetchMovies = async () => {
+      const response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=ea962e6c660af4bd263293356299c277');
+      const data = await response.json();
+      console.log(data);
+    }
+    fetchMovies();
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world!
     </div>
   );
 }
