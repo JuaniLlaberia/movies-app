b398e83f7c84ec8b0321fb1b39fb18488025e849
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-05-12 14:07:36 +0200

    Hiding API key

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'ea962e6c660af4bd263293356299c277',
+    apiKey: process.env.REACT_APP_API_KEY,
     posterImgUrl: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     bgImgUrl: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
